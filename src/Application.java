515c96369b8a58691c5400214d421619e7c0f48b
s3604139 <s3604139@student.rmit.edu.au>
2018-04-08 18:13:57 +1000

    Update Application.java

--- src/Application.java
+++ src/Application.java
@@ -1,10 +1,19 @@
 import accounts.*;
+import schedualing.*;
 
 public class Application {
 
 	public static void main(String[] args) {
-	Admin testAdmin=new Admin("login","pword");
-	testAdmin.login("pword");
 	User_Interface system=new User_Interface();
+	system.accounts.addAccount(new Admin("login","pword"));
+	system.accounts.addAccount(new Staff("staff","pword"));
+	
+	for(Account account:system.accounts.accounts)
+	{
+		System.out.println(account._Login);
+	}
+	System.out.println("'Schedual' for schedual");
+	system.login();
+	system.ShowSchedual(system.schedual);
 	}
 }
